ce8aecfb62d8c988942a31ffa1e99393930bc41e
Brecht <brechtdv@gmail.com>
2021-11-04 19:34:15 +0100

    add plot

--- GBD2019/02_analyses/02-GBD2019-BE-LE.R
+++ GBD2019/02_analyses/02-GBD2019-BE-LE.R
@@ -9,6 +9,8 @@
 #' # Settings
 
 ## required packages
+library(cowplot)
+library(ggplot2)
 library(knitr)
 
 ## define EU-15 countries
@@ -83,11 +85,61 @@ if (length(unique(x$location)) != 15)
 
 #' # Life Expectancy
 
-#' ## BENCHMARK EU-15 MEAN
+#' ## Benchmark tables
 #+ results='asis'
 
 tab_eu15_mean(1990, 2019, "Male")
 tab_eu15_mean(1990, 2019, "Female")
 tab_eu15_mean(1990, 2019, "Both")
+
+#' ## Benchmark plot
+
+x1 <- subset(x, year %in% range(x$year) & sex == "Male")
+x1 <- x1[order(x1$location), ]
+x1$val2 <- x1$val
+x1[x1$year == max(x$year), ]$val2 <-
+  subset(x1, year == max(x$year))$val - subset(x1, year == min(x$year))$val
+
+x1 <- x1[order(x1$val), ]
+x1$location <-
+  factor(x1$location, subset(x1, year == max(x$year))$location)
+x1$year <- factor(x1$year, range(x$year))
+x1$sex <- "Men"
+
+p1 <-
+ggplot(x1, aes(x = val2, y = location, fill = year)) +
+  geom_col(position = position_stack(reverse = TRUE)) +
+  facet_grid(~sex) +
+  scale_fill_brewer() +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  labs(x = "Life expectancy", y = NULL, fill = NULL)
+
+x2 <- subset(x, year %in% range(x$year) & sex == "Female")
+x2 <- x2[order(x2$location), ]
+x2$val2 <- x2$val
+x2[x2$year == max(x$year), ]$val2 <-
+  subset(x2, year == max(x$year))$val - subset(x2, year == min(x$year))$val
+
+x2 <- x2[order(x2$val), ]
+x2$location <-
+  factor(x2$location, subset(x2, year == max(x$year))$location)
+x2$year <- factor(x2$year, range(x$year))
+x2$sex <- "Women"
+
+p2 <-
+ggplot(x2, aes(x = val2, y = location, fill = year)) +
+  geom_col(position = position_stack(reverse = TRUE)) +
+  facet_grid(~sex) +
+  scale_fill_brewer() +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  labs(x = "Life expectancy", y = NULL, fill = NULL)
+
+plot_grid(p1, p2)
+
+tiff("Fig1.tiff", 10, 4, units = "in", res = 300, compress = "lzw")
+plot_grid(p1, p2)
+dev.off()
 
 ##rmarkdown::render("02-GBD2019-BE-LE.R")
